9ee52ad3abae487465900c2c078fa1410879d74a
sadFrog <one.618phi.yahoo.co.uk>
2017-02-15 17:46:49 +0000

    commit3

--- git-eclispse/src/GitEclipse.java
+++ git-eclispse/src/GitEclipse.java
@@ -8,8 +8,9 @@ public class GitEclipse {
 		System.out.println("from Git  Repository, Clone a Git repository");
 		System.out.println("paste url, Next-Next-Finish");
 		System.out.println("right click project, Team-Share Project (confirm locations)");
-		System.out.println("right click project, Team-Commit");
+		System.out.println("right click project, Team-Commit, save changes");
 		System.out.println("in Git Ropositories right click master in local branch and push branch");
+		System.out.println("OR right click project, Team-Push Branch Master");
 	}
 
 }
