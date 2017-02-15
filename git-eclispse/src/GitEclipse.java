159c49d795a637aeed6874a946d88313d540a6c2
sadFrog <one.618phi.yahoo.co.uk>
2017-02-15 17:33:12 +0000

    first commit

---
+++ git-eclispse/src/GitEclipse.java
@@ -0,0 +1,14 @@
+
+public class GitEclipse {
+
+	public static void main(String[] args) {
+		System.out.println("create new repo with readme and ignore on github, copy url");
+		System.out.println("Windows-Preferences (add user.name, user.email)");
+		System.out.println("Window-Show View-Other-Git  Repository");
+		System.out.println("from Git  Repository, Clone a Git repository");
+		System.out.println("paste url, Next-Next-Finish");
+		System.out.println("right click project, Team-Share Project (confirm locations)");
+		System.out.println("right click project, Team-Commit");
+	}
+
+}
