979e8dde2e811506e1be5d4f7cb622d3b239eec4
sadFrog <one.618phi.yahoo.co.uk>
2017-04-08 07:04:02 +0100

    new change

--- git-eclispse/src/GitEclipse.java
+++ git-eclispse/src/GitEclipse.java
@@ -4,8 +4,8 @@ public class GitEclipse {
 	public static void main(String[] args) {
 		System.out.println("create new repo with readme and ignore on github, copy url");
 		System.out.println("Window >Preferences > Team > Git > Configuration (add user.name, user.email)");
-		System.out.println("Window > Show View > Other > Git  Repository");
-		System.out.println("from Git  Repository, Clone a Git repository");
+		System.out.println("Window > Show View > Other > Git  Repository  (opens side window)");
+		System.out.println("(from side window) Git  Repository, Clone a Git repository");
 		System.out.println("paste url, Next > Next > Finish");
 		System.out.println("right click project, Team > Share Project (confirm locations)");
 		System.out.println("right click project, Team > Commit, Save Changes > Commit");
