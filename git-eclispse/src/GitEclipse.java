2a8a28126ea95562ca2035c81cefb0dc0bd65dfa
sadFrog <one.618phi.yahoo.co.uk>
2017-04-08 06:15:42 +0100

    adjustment

--- git-eclispse/src/GitEclipse.java
+++ git-eclispse/src/GitEclipse.java
@@ -3,14 +3,14 @@ public class GitEclipse {
 
 	public static void main(String[] args) {
 		System.out.println("create new repo with readme and ignore on github, copy url");
-		System.out.println("Windows-Preferences (add user.name, user.email)");
-		System.out.println("Window-Show View-Other-Git  Repository");
+		System.out.println("Window >Preferences > Team > Git > Configuration (add user.name, user.email)");
+		System.out.println("Window > Show View > Other > Git  Repository");
 		System.out.println("from Git  Repository, Clone a Git repository");
-		System.out.println("paste url, Next-Next-Finish");
-		System.out.println("right click project, Team-Share Project (confirm locations)");
-		System.out.println("right click project, Team-Commit, Save Changes-Commit");
+		System.out.println("paste url, Next > Next > Finish");
+		System.out.println("right click project, Team > Share Project (confirm locations)");
+		System.out.println("right click project, Team > Commit, Save Changes > Commit");
 		System.out.println("in Git Ropositories right click master in local branch and push branch");
-		System.out.println("OR right click project, Team-Push Branch Master");
+		System.out.println("OR right click project, Team > Push Branch Master");
 	}
 
 }
